f11acbcb4a35cdb6e283ec27e7cb2e36ec8194ff
Konstantin Sokolov <112500164+Konstantinsokol@users.noreply.github.com>
2023-10-19 23:50:24 +0300

    Update imu_sensor.py

--- for git/sensor_pkg/scripts/imu_sensor.py
+++ for git/sensor_pkg/scripts/imu_sensor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import rospy
-from geometry_msgs.msg import TwistWithCovarianceStamped
+from sensor_msgs.msg import Imu
 import os
 import sys
 import time
@@ -10,33 +10,25 @@ import numpy as np
 from imusensor.MPU9250 import MPU9250
 from imusensor.filters import kalman 
 
-def composeMessage(count, roll, pitch, yaw, accel_x, accel_y, accel_z):
+def composeMessage(count, angular_z, accel_x, accel_y):
   
   msg.header.stamp = rospy.Time.now()
   msg.header.frame_id = "base_link"
   msg.header.seq = count
 
-  msg.twist.twist.linear.x = accel_x
-  msg.twist.twist.linear.y = accel_y
-  msg.twist.twist.linear.z = accel_z
+  msg.linear_acceleration.x = accel_x
+  msg.linear_acceleration.y = accel_y
+  
+  msg.angular_velocity.z = angular_z
 
-  msg.twist.twist.angular.x = roll
-  msg.twist.twist.angular.y = pitch
-  msg.twist.twist.angular.z = yaw
-  msg.twist.covariance = [0.001, 0, 0, 0, 0, 0,
-                          0, 0.001, 0, 0, 0, 0,
-                          0, 0, 0.001, 0, 0, 0,
-                          0, 0, 0, 0.001, 0, 0,
-                          0, 0, 0, 0, 0.001, 0,
-                          0, 0, 0, 0, 0, 0.001]
   imu_sensor.publish(msg)
 
 if __name__ == "__main__":
 
   rospy.init_node('imu_sensor')
   r = rospy.Rate(60)
-  imu_sensor = rospy.Publisher('/imu_sensor_link', TwistWithCovarianceStamped, queue_size=10)
-  msg = TwistWithCovarianceStamped()
+  imu_sensor = rospy.Publisher('/imu_sensor_link', Imu, queue_size=10)
+  msg = Imu()
   address = 0x68
   bus = smbus.SMBus(1)
   imu = MPU9250.MPU9250(bus, address)
@@ -45,9 +37,6 @@ if __name__ == "__main__":
   imu.loadCalibDataFromFile("/home/ubuntu/Roboprint/Autriger/python/calib1.json")
   imu.readSensor()
   imu.computeOrientation()
-  sensorfusion.roll = imu.roll
-  sensorfusion.pitch = imu.pitch
-  sensorfusion.yaw = imu.yaw
 
   while not rospy.is_shutdown():
     count = 0
@@ -61,6 +50,6 @@ if __name__ == "__main__":
     sensorfusion.computeAndUpdateRollPitchYaw(imu.AccelVals[0], imu.AccelVals[1], imu.AccelVals[2], imu.GyroVals[0], imu.GyroVals[1], imu.GyroVals[2],\
                           imu.MagVals[0], imu.MagVals[1], imu.MagVals[2], dt)
  
-    composeMessage(count, sensorfusion.pitch, sensorfusion.roll, imu.yaw+40,imu.AccelVals[1], imu.AccelVals[0], imu.AccelVals[2])
+    composeMessage(-count,imu.GyroVals[2] ,-imu.AccelVals[1], -imu.AccelVals[0])
     count += 1
     r.sleep()
